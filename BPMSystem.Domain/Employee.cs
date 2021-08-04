cbc91ec4053ff57b746819985335da52ac2acf8c
LatyshevViktor <latyshev.vicktor@yandex.ru>
2021-08-04 14:35:11 +0300

    Создание класса "Сотрудник"

--- BPMSystem.Domain/Employee.cs
+++ BPMSystem.Domain/Employee.cs
@@ -2,7 +2,28 @@
 
 namespace BPMSystem.Domain
 {
+    // Класс описывающий сотрудника
     public class Employee
     {
+        /// <summary>
+        /// Id сотрудника
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+        /// <summary>
+        /// Дата найма на работу
+        /// </summary>
+        public DateTime? DateOfWork { get; set; }
     }
 }
