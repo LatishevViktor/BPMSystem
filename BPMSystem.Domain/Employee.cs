b5dbcd3cb37b980bdc327834d72662a9f898e7ab
LatyshevViktor <latyshev.vicktor@yandex.ru>
2021-08-05 14:48:47 +0300

    Изменил класс Employee

--- BPMSystem.Domain/Employee.cs
+++ BPMSystem.Domain/Employee.cs
@@ -1,29 +1,47 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BPMSystem.Domain
 {
     // Класс описывающий сотрудника
     public class Employee
     {
-        /// <summary>
-        /// Id сотрудника
-        /// </summary>
+        [Key]
         public Guid Id { get; set; }
+
         /// <summary>
         /// Имя
         /// </summary>
         public string FirstName { get; set; }
+
         /// <summary>
         /// Фамилия
         /// </summary>
         public string LastName { get; set; }
+
         /// <summary>
         /// Дата рождения
         /// </summary>
         public DateTime? DateOfBirth { get; set; }
+
         /// <summary>
         /// Дата найма на работу
         /// </summary>
         public DateTime? DateOfWork { get; set; }
+
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string Position { get; set; }
+
+        /// <summary>
+        /// Опыт работы
+        /// </summary>
+        public string WorkExperience { get; set; }
+
+        /// <summary>
+        /// Табельный номер
+        /// </summary>
+        public string PersonNumber { get; set; }
     }
 }
