84f7c32b1766c7056433757ca582978b0dbe8286
LatishevViktor <66080751+LatishevViktor@users.noreply.github.com>
2021-09-27 16:45:23 +0300

    2021-09-27-rev01
    Добавил иконки, и немного поменял стилизацию. Добавил название ссылок, по которым будет идти переход на другие страницы

--- BMPSystem.Client/ClientAngular/src/app/app.module.ts
+++ BMPSystem.Client/ClientAngular/src/app/app.module.ts
@@ -4,17 +4,20 @@ import {MatSliderModule } from '@angular/material/slider'
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon'
 import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    FormsModule
+    FormsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
