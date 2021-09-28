3f9260a92cb7266576795f14d0ab425eebf5ae36
LatishevViktor <66080751+LatishevViktor@users.noreply.github.com>
2021-09-28 17:09:38 +0300

    2021-09-28-rev02
    Заменил кнопки на mat-nav-list и добавил автоматическое закрытие бокового меню при нажатии на ссылку. Добавил новые компоненты

--- BMPSystem.Client/ClientAngular/src/app/app-routing.module.ts
+++ BMPSystem.Client/ClientAngular/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { PositionComponent } from './position/position.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'department', component: DepartmentComponent}
+  {path: 'department', component: DepartmentComponent},
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'position', component: PositionComponent}
 ];
 
 @NgModule({
