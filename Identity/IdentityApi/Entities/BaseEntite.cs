9d19877a81803f70b2c76b908a384f332ac9f1d9
LatishevViktor <66080751+LatishevViktor@users.noreply.github.com>
2021-10-24 00:04:08 +0300

    24-10-2021-rev01
    Добавил проект для авторизации и регистрации, создал новую БД с таблицей User

---
+++ Identity/IdentityApi/Entities/BaseEntite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityApi.Entities
+{
+    public class BaseEntite
+    {
+        [Key]
+        public long UserId { get; set; }
+    }
+}
