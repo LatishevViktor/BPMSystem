b395d09ffe3b4adb7497579666c01ba1476fd765
LatishevViktor <66080751+LatishevViktor@users.noreply.github.com>
2021-11-18 15:49:39 +0300

    2021-11-18-rev01
    Создал авторизационный сервис и почистил класс Startup в Web проекте

---
+++ BPMSystem.Web/Extensions services/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using BPMSystem.BLL.Interfaces;
+using BPMSystem.BLL.Services;
+using BPMSystem.DAL.Interfaces;
+using BPMSystem.DAL.Repositories;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Services.BPMSystemBLL.Services;
+
+namespace BPMSystem.Web.Extensions_services
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCorsCustom(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("BpmServicePolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+        }
+
+        public static void AddRegistrationServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<IDepartmentService, DepartmentService>();
+
+            services.AddTransient<IPositionRepository, PositionRepository>();
+            services.AddTransient<IPositionService, PositionService>();
+
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IEmployeeService, EmployeeService>();
+        }
+    }
+}
