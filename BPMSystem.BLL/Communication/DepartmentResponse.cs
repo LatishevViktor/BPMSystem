90159dd7021e53faaaf85b43d5469c9afe9e4ed9
mausermuz <69821216+mausermuz@users.noreply.github.com>
2022-01-10 20:32:26 +0300

    Create response

---
+++ BPMSystem.BLL/Communication/DepartmentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BPMSystem.Web.Communication
+{
+    public class DepartmentResponse : BaseResponse
+    {
+        public dynamic Departments { get; set; } = null!;
+        public DepartmentResponse(bool success, string message, dynamic departments) : base(success, message)
+        {
+            Departments = departments;
+        }
+    }
+}
